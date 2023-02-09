d5d75d93b282fc8bfc7518f84cf52b3ee779a5e8
Volutar <volutar@mail.ru>
2023-02-09 21:02:53 +0500

    Added support of >80 tracks SCL/TRD

--- src/libxpeccy/filetypes/scl.c
+++ src/libxpeccy/filetypes/scl.c
@@ -52,6 +52,7 @@ int loadSCL(Computer* comp, const char* name, int drv) {
 		buf[0x8e3] = 0x16;			// 80DS
 		buf[0x8e4] = hd.files;			// files total
 		tmpa = 0x9f0 - scnt;			// sectors free (0x9f0)	// FIXED: not 0xa00!
+		if (tmpa < 0) tmpa = 0x20 - (scnt & 0x1f);
 		buf[0x8e5] = (tmpa & 0xff);
 		buf[0x8e6] = ((tmpa & 0xff00) >> 8);
 		buf[0x8e7] = 0x10;			// trdos code
@@ -65,7 +66,7 @@ int loadSCL(Computer* comp, const char* name, int drv) {
 			i++;
 		}
 		memset(buf, 0, 0x1000);
-		while (i < 160) {
+		while (i < 256) {
 			flp_format_trk(flp, i, 16, 256, (char*)buf);
 			i++;
 		}
@@ -78,7 +79,7 @@ int loadSCL(Computer* comp, const char* name, int drv) {
 int saveSCL(Computer* comp, const char* name, int drv) {
 	Floppy* flp = comp->dif->flp[drv & 3];
 	const char* sign = "SINCLAIR";
-	unsigned char img[0xA0000];
+	unsigned char img[0x100000];
 	unsigned char buf[256];
 	unsigned char* dptr;
 	unsigned char* bptr;
--- src/libxpeccy/filetypes/trd.c
+++ src/libxpeccy/filetypes/trd.c
@@ -1,12 +1,12 @@
 #include "filetypes.h"
 
 int loadTRD(Computer* comp, const char* name, int drv) {
-	Floppy* flp = comp->dif->flp[drv & 3];
+	Floppy* flp = comp->dif->fdc->flop[drv & 3];
 	FILE* file = fopen(name, "rb");
 	if (!file) return ERR_CANT_OPEN;
 	int err = ERR_OK;
 	size_t len = fgetSize(file);
-	if (((len & 0xff) != 0) || (len == 0) || (len > 0xa8000)) {
+	if (((len & 0xff) != 0) || (len == 0) || (len > 0xfe000)) { // 127 tracks
 		err = ERR_TRD_LEN;
 	} else {
 //		diskFormat(flp);
@@ -18,7 +18,7 @@ int loadTRD(Computer* comp, const char* name, int drv) {
 			trk++;
 		} while  (!feof(file));
 		memset(buf, 0, 0x1000);
-		while (trk < 160) {
+		while (trk < 168) {
 			flp_format_trk(flp, trk, 16, 256, buf);
 			trk++;
 		}
@@ -30,10 +30,20 @@ int loadTRD(Computer* comp, const char* name, int drv) {
 }
 
 int saveTRD(Computer* comp, const char* name, int drv) {
-	Floppy* flp = comp->dif->flp[drv & 3];
-	unsigned char img[0xa8000];
+	Floppy* flp = comp->dif->fdc->flop[drv & 3];
+	unsigned char syssec[0x100];
+	diskGetSectorData(flp, 0, 9, syssec, 256);
+	int disksize;
+	if (syssec[0xe7] == 0x10)
+		disksize = syssec[0xe1] + syssec[0xe2]*16 + syssec[0xe5] + syssec[0xe6]*256;
+	else
+		disksize = 0;
+	if (disksize < 2560) disksize = 2560; //80*2*16
+	int tracks = (disksize + 15) / 16;
+
+	unsigned char img[disksize*0x100];
 	unsigned char* dptr = img;
-	for (int i = 0; i < 160; i++) {
+	for (int i = 0; i < tracks; i++) {
 		for (int j = 1; j < 17; j++) {
 			if (!diskGetSectorData(flp, i, j, dptr, 256)) {
 				return ERR_TRD_SNF;
@@ -44,7 +54,7 @@ int saveTRD(Computer* comp, const char* name, int drv) {
 
 	FILE* file = fopen(name, "wb");
 	if (!file) return ERR_CANT_OPEN;
-	fwrite((char*)img, 0xa0000, 1, file);
+	fwrite((char*)img, disksize*0x100, 1, file);
 	fclose(file);
 
 	flp_set_path(flp, name);
